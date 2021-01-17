447befae43216f7c1f0dbb1a7176d767681d182f
Alexandr Sirotkin <aasirotkin@mail.ru>
2021-01-17 16:30:51 +0300

    Update geo with blank

--- src/geo.h
+++ src/geo.h
@@ -1,1 +1,17 @@
 #pragma once
+
+#include <cmath>
+
+struct Coordinates {
+    double lat;
+    double lng;
+};
+
+inline double ComputeDistance(Coordinates from, Coordinates to) {
+    using namespace std;
+    static const double dr = 3.1415926535 / 180.0;
+    static const double rz = 6371000.0;
+    return acos(
+        sin(from.lat * dr) * sin(to.lat * dr) +
+        cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr)) * rz;
+}
