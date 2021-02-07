4f6f3a80489145a64d28627f5adaee8a7fee81fd
Alexandr Sirotkin <aasirotkin@mail.ru>
2021-02-07 11:22:43 +0300

    Remove old fasion test

--- src/test_example_functions.cpp
+++ src/test_example_functions.cpp
@@ -187,94 +187,12 @@ void TestParseGeoFromStringView() {
     }
 }
 
-// Функция TestQueriesEngine является точкой входа прохождения тестов платформы
-void TestQueriesEngine(std::istream& input_ss, std::ostream& output_ss) {
-    TransportCatalogue catalogue;
-
-    int query_count = 0;
-
-    query_count = ReadLineWithNumber(input_ss);
-    std::vector<std::string> queries;
-    queries.reserve(query_count);
-    for (int i = 0; i < query_count; ++i) {
-        std::string query = ReadLine(input_ss);
-        queries.push_back(query);
-    }
-    InputQueries(catalogue, queries);
-
-    query_count = ReadLineWithNumber(input_ss);
-    for (int i = 0; i < query_count; ++i) {
-        std::string query = ReadLine(input_ss);
-        OutputQuery(catalogue, query, output_ss);
-    }
-}
-
-void TestQueries(const vector<string>& input, const vector<string>& output) {
-    stringstream input_ss;
-    for (const string& input_i : input) {
-        input_ss << input_i;
-    }
-
-    stringstream output_ss;
-    for (const string& output_i : output) {
-        output_ss << output_i;
-    }
-
-    stringstream result_ss;
-    TestQueriesEngine(input_ss, result_ss);
-
-    string result_string;
-    string output_string;
-
-    ASSERT_EQUAL(result_ss.str().size(), output_ss.str().size());
-    while (getline(result_ss, result_string) && getline(output_ss, output_string)) {
-        ASSERT_EQUAL(result_string, output_string);
-    }
-}
-
-void TestPlatformQueries() {
-    const vector<string>& input = {
-        "13\n"s,
-        "Stop Tolstopaltsevo: 55.611087, 37.20829, 3900m to Marushkino\n"s,
-        "Stop Marushkino: 55.595884, 37.209755, 9900m to Rasskazovka, 100m to Marushkino\n"s,
-        "Bus 256: Biryulyovo Zapadnoye > Biryusinka > Universam > Biryulyovo Tovarnaya > Biryulyovo Passazhirskaya > Biryulyovo Zapadnoye\n"s,
-        "Bus 750: Tolstopaltsevo - Marushkino - Marushkino - Rasskazovka\n"s,
-        "Stop Rasskazovka: 55.632761, 37.333324, 9500m to Marushkino\n"s,
-        "Stop Biryulyovo Zapadnoye: 55.574371, 37.6517, 7500m to Rossoshanskaya ulitsa, 1800m to Biryusinka, 2400m to Universam\n"s,
-        "Stop Biryusinka: 55.581065, 37.64839, 750m to Universam\n"s,
-        "Stop Universam: 55.587655, 37.645687, 5600m to Rossoshanskaya ulitsa, 900m to Biryulyovo Tovarnaya\n"s,
-        "Stop Biryulyovo Tovarnaya: 55.592028, 37.653656, 1300m to Biryulyovo Passazhirskaya\n"s,
-        "Stop Biryulyovo Passazhirskaya: 55.580999, 37.659164, 1200m to Biryulyovo Zapadnoye\n"s,
-        "Bus 828: Biryulyovo Zapadnoye > Universam > Rossoshanskaya ulitsa > Biryulyovo Zapadnoye\n"s,
-        "Stop Rossoshanskaya ulitsa: 55.595579, 37.605757\n"s,
-        "Stop Prazhskaya: 55.611678, 37.603831\n"s,
-        "6\n"s,
-        "Bus 256\n"s,
-        "Bus 750\n"s,
-        "Bus 751\n"s,
-        "Stop Samara\n"s,
-        "Stop Prazhskaya\n"s,
-        "Stop Biryulyovo Zapadnoye\n"s
-    };
-
-    const vector<string>& output = {
-        "Bus 256: 6 stops on route, 5 unique stops, 5950 route length, 1.36124 curvature\n"s,
-        "Bus 750: 7 stops on route, 3 unique stops, 27400 route length, 1.30853 curvature\n"s,
-        "Bus 751: not found\n"s,
-        "Stop Samara: not found\n"s,
-        "Stop Prazhskaya: no buses\n"s,
-        "Stop Biryulyovo Zapadnoye: buses 256 828\n"s
-    };
-
-    TestQueries(input, output);
-}
-
 // --------- Окончание модульных тестов -----------
 
 // Функция TestTransportCatalogue является точкой входа для запуска тестов
 void TestTransportCatalogue() {
     RUN_TEST(TestParseGeoFromStringView);
-    RUN_TEST(TestPlatformQueries);
+
 #ifndef _DEBUG
 
 #endif
